4d2eb0be971fc85159ea8475865aee91bec77860
Harish <135962503+Harish-S28@users.noreply.github.com>
2026-03-16 14:13:58 +0530

    Add TripRepository interface for Trip entity

--- backend/src/main/java/com/travelapp/repository/TripRepository.java
+++ backend/src/main/java/com/travelapp/repository/TripRepository.java
@@ -1,1 +1,8 @@
+package com.travelapp.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.travelapp.model.Trip;
+
+public interface TripRepository extends JpaRepository<Trip, Integer> {
+
+}
